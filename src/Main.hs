8ec655128c16f2028a2bb6eaf266ec670004cebb
Andy Gill <andygill@ku.edu>
2013-04-06 12:07:43 -0500

    Adding some TH to help us derive things.

--- src/Main.hs
+++ src/Main.hs
@@ -1,18 +1,37 @@
-{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, TypeFamilies, TemplateHaskell #-}
 
 module Main where
 
 import Data.Default ( Default(..) )
 import Data.Semigroup ( (<>) )
-import Control.Monad ( liftM2 )
+import Control.Monad
 import Data.Boolean
+import Data.Boolean.Numbers as N
+import Data.Proxy
 
 import Language.Sunroof
 import Language.Sunroof.Server
+import Language.Sunroof.Compiler
 import Language.Sunroof.JS.Canvas
-import Language.Sunroof.JS.Browser ( alert )
+import Language.Sunroof.JS.Browser as B
 import Language.Sunroof.JS.JQuery
 import Network.Wai.Middleware.Static
+
+import TH
+
+type  Slide2 = (JSString,JSNumber)
+
+$(jstuple ''Slide2)
+
+newtype Slide = Slide JSObject
+
+derive [d| instance Show Slide |]
+
+newtype JSX a = JSX JSObject
+
+-- | Show the Javascript.
+derive [d| instance (SunroofArgument o) => Show (JSX o) |]
+derive [d| instance (SunroofArgument o) => Sunroof (JSX o) |]
 
 ourPolicy :: Policy -> Policy
 ourPolicy p = p
@@ -29,7 +48,6 @@ main = do
                       }) $ \ doc -> asyncJS doc prog
 
 
-newtype Slide = Slide JSObject
 
 instance Sunroof Slide where
   box   = Slide . box
@@ -42,18 +60,100 @@ instance JSTuple Slide where
   tuple (id_,num) = do
     o <- new "Object" ()
     o # "id" := id_
-    o # "num" := num
+    o # "value" := num
     return $ Slide o
 
-prog :: JSB ()
+newtype JSViewPort = JSViewPort JSObject
+
+instance Sunroof JSViewPort where
+  box   = JSViewPort . box
+  unbox (JSViewPort o) = unbox o
+
+data ViewPort = ViewPort
+        { vpPage  :: JSNumber   -- page number, first page is page zero
+        , vpX     :: JSNumber
+        , vpY     :: JSNumber
+        , vpScale :: JSNumber   -- scale
+        }
+
+instance JSTuple JSViewPort where
+  type Internals JSViewPort = ViewPort
+  match o = ViewPort
+                { vpPage  = o ! "page"
+                , vpX     = o ! "x"
+                , vpY     = o ! "y"
+                , vpScale = o ! "scale"
+                }
+  tuple vp = do
+    o <- new "Object" ()
+    o # "page"  := vpPage  vp
+    o # "x"     := vpX     vp
+    o # "y"     := vpY     vp
+    o # "scale" := vpScale vp
+    return $ JSViewPort o
+
+-- how big do we scale our pdf picture, in dots per inch?
+dpi :: JSNumber
+dpi = 300
+
+prog :: JSA ()
 prog = do
+      -- set up the slider listener
       ch <- newChan
-
       jq "body" >>= on "slide" ".slide" (\ (a :: JSObject, aux :: JSObject) -> do
-                the_id :: JSString <- jq (cast $ this) >>= invoke "attr" ("id" :: JSString)
-                tuple (the_id,aux ! "value") >>= \ (o :: Slide) -> ch # writeChan o)
+                the_id    :: JSString <- jq (cast $ this) >>= invoke "attr" ("id" :: JSString)
+                the_index :: JSNumber <- evaluate $ N.round (aux ! "value" :: JSNumber)
+                tuple (the_id,the_index) >>= \ (o :: Slide) -> ch # writeChan o)
 
-      alert("Hello")
+      forkJS $ loop () $ \ () -> do
+        o :: Slide <- ch # readChan
+        let (the_id,aux) = match o
+        console # B.log ("Slide : " <> cast the_id <> " " <> cast aux :: JSString)
+        return ()
+
+      -- channel of what to show on the graphical viewport
+      viewportChan :: JSChan JSViewPort <- newChan
+
+
+      -- global of the current viewer state
+      vp :: JSViewPort <- tuple $ ViewPort 0 400 100 2.0
+      viewerState :: JSMVar JSViewPort <- newMVar vp
+
+
+      -- global state changers
+      setPage :: JSContinuation JSNumber <- continuation $ \ (n :: JSNumber) -> do
+              vp :: ViewPort <- (viewerState # takeMVar) >>= (return . match)
+              let vp' = vp { vpPage = n }
+              return ()
+
+      -- The cached image, a non-connected DOM object, that is also an <img>
+      imageObj :: JSObject <- new "Image" ()
+
+      canvas <- document # getElementById("myCanvas")
+      context <- canvas # getContext("2d")
+
+      drawViewPort <- continuation $ \ () -> do
+              o <- viewportChan # readChan
+              let vp = match o
+
+              -- figure out if we have the correct image loaded
+              -- TODO
+
+              -- load new image object
+              imageObj # src := "pages/exam.300-2.png";
+
+              jq (cast $ imageObj) >>= on "load" "" (\ () -> do
+                        alert("loaded image")
+                        context # drawImageClip imageObj (400, 100) (2050, 1490) (0, 0) (960, 600))
+
+              return ()
+
+      forkJS (goto drawViewPort () :: JSB ())
+
+      viewportChan # writeChan vp
+
+      return ()
+
 
 {-
       jq "body" >>= on "click" ".click" (\ () -> do
@@ -108,4 +208,17 @@ prog = do
 
 default(JSNumber, JSString, String)
 
+----------------------------------------------------------------
 
+test1 :: forall o . JSTuple o => Proxy o -> IO ()
+test1 Proxy = do
+        txt <- sunroofCompileJSA def "main" $ do
+                o :: o <- liftM cast $ new "Test" ()
+                o' <- fn1 o
+                alert(cast o' :: JSString)
+        putStrLn txt
+
+fn1 :: JSTuple o => o -> JS t o
+fn1 o = do
+        o' <- tuple (match o)
+        return o'
---
+++ src/TH.hs
@@ -0,0 +1,144 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, TypeFamilies, TemplateHaskell #-}
+
+module TH where
+
+import Language.Haskell.TH
+import Language.Sunroof.Types as SRT
+import Language.Sunroof.Classes
+import Language.Sunroof.JS.Object
+
+-- | @derive@ derives incomplete instances.
+--
+-- you write the newtype explictly, and @derive@ does the rest.
+--
+-- > newtype JSX = JSX JSObject
+--
+-- and then the start of the instance, and the rest gets filled in
+--
+-- > derive [d| instance Show JSX |]
+--
+-- generates
+--
+-- > instance Show JSX where
+-- >    show (JSX o) = show o
+--
+
+derive :: Q [Dec] -> Q [Dec]
+derive decsQ = do
+        decs <- decsQ
+        mapM complete decs
+  where
+        complete :: Dec -> Q Dec
+        complete (InstanceD cxt hd@(AppT (ConT typeClass) ty) decls) = do
+                let k decls' = InstanceD cxt hd (decls ++ decls')
+                let findClass (ConT t) = t
+                    findClass (AppT t1 _) = findClass t1
+                    findClass _ =  error $ "strange instance head found in derive " ++ show ty
+                let tConTy = findClass ty
+                info <- reify tConTy
+                case info of
+                    TyConI (NewtypeD _ _ _ (NormalC tCons [(NotStrict,ConT o)]) [])
+                        | o == ''JSObject -> do
+                                decls' <- completeWith typeClass tConTy tCons
+                                return $ k decls'
+                        | otherwise -> error $ "not newtype of JSObject"
+                    _ -> error $ "strange info for newtype type " ++ show info
+        complete dec = error $ "can not complete " ++ show dec
+
+        completeWith :: Name -> Name -> Name -> Q [Dec]
+        completeWith tyClass tConTy tCons
+           | tyClass == ''Show = do
+                o <- newName "o"
+                return [ FunD 'show
+                              [ Clause [ConP tCons [VarP o]]
+                                         (NormalB (AppE (VarE 'show) (VarE o))) []]]
+           | tyClass == ''Sunroof = do
+                o <- newName "o"
+                n <- newName "n"
+                return [ FunD 'box
+                          [ Clause [VarP n] (NormalB (AppE (ConE tCons)
+                                                           (AppE (VarE 'box) (VarE n)))) []]
+                       , FunD 'unbox
+                          [ Clause [ConP tCons [VarP o]]
+                                            (NormalB (AppE (VarE 'unbox) (VarE o))) []]
+                       ]
+           | otherwise = error $ "trying to complete " ++ show tyClass
+
+jstuple :: Name -> Q [Dec]
+jstuple nm = do
+        info <- reify nm
+        case info of
+          TyConI (TySynD _ [] tyRhs) -> js2 (unroll tyRhs [])
+          _ -> error $ "jstuple: " ++ Prelude.show nm ++ " is not a type constructor, it is a : " ++ Prelude.show info
+  where
+        -- The new name of the JS-version of the structure
+        jsName :: Name
+        jsName = mkName $ "JS" ++ nameBase nm
+
+        js2 :: (Type,[Type]) -> Q [Dec]
+        js2 (t@(TupleT n),ts) | length ts == n = do
+                runIO $ print $ jsName
+                o <- newName "o"
+                let field_name :: [(String,Type)]
+                    field_name = [ ("f" ++ Prelude.show n ,t) | (n::Int,t) <- zip [1..] ts ]
+
+                vs <- sequence [ newName "v" | _ <- ts ]
+
+                return
+                   [ NewtypeD
+                        []
+                        jsName
+                        []
+                        (NormalC jsName [(NotStrict,ConT $ mkName "JSObject")])
+                        []
+                      -- this is a simple wrapper roound JSObject
+                   , InstanceD
+                        []
+                        (AppT (ConT ''Sunroof) (ConT $ jsName))
+                        [ FunD 'box
+                          [ Clause [VarP o] (NormalB (AppE (ConE jsName)
+                                                           (AppE (VarE 'box) (VarE o)))) []]
+                        , FunD 'unbox
+                          [ Clause [ConP jsName [VarP o]]
+                                            (NormalB (AppE (VarE 'unbox) (VarE o))) []]
+                        ]
+                   , InstanceD
+                        []
+                        (AppT (ConT ''JSTuple) (ConT $ jsName))
+                        [ TySynInstD ''Internals [ConT jsName] (foldl AppT t ts)
+
+                        , FunD 'SRT.match
+                          [ Clause [VarP o] (NormalB (TupE
+                                        [ SigE (AppE (AppE (VarE $ mkName "!") (VarE o))
+                                                           (AppE (VarE 'attr) (LitE $ StringL $ f_nm)))
+                                               t
+                                        | (f_nm,t) <- field_name
+                                        ])) []
+                          ]
+                        , FunD 'SRT.tuple
+                          [ Clause [TupP $ map VarP vs] (NormalB (DoE (
+                                        [ BindS (VarP o) (AppE (AppE (VarE 'new) (LitE $ StringL $ "Object")) (TupE []))
+                                        ] ++
+                                        [ NoBindS $
+                                          let assign = AppE (AppE (ConE $ mkName ":=")
+                                                                  (AppE (VarE 'attr) (LitE $ StringL $ f_nm)))
+                                                            (VarE v)
+
+                                          in  AppE (AppE (VarE $ mkName "#")
+                                                         (VarE o))
+                                                   (assign)
+                                        | ((f_nm,t),v) <- field_name `zip` vs
+                                        ] ++
+                                        [ NoBindS $ AppE (VarE 'return) (AppE (ConE jsName) (VarE o))
+                                        ]))) []
+                          ]
+
+                        ]
+                   ]
+        js2 (o,_) = error $ "jstuple: " ++ Prelude.show o ++ " is not a tuple"
+
+        unroll :: Type -> [Type] -> (Type,[Type])
+        unroll (AppT t1 t2) ts = unroll t1 (t2 : ts)
+        unroll other ts        = (other,ts)
+
+--  ''Slide ''JSSlide
